dbe99de5efd52a20725807bcde24f97e4fe79592
travis.terrell <travis.terrell@inuvo.com>
2015-02-03 17:11:46 -0600

    Progress1

---
+++ Projects/Inuvo/CategoryManager/CategoryManager/app/CategoryManagerApp.js
@@ -0,0 +1,15 @@
+﻿(function() {
+
+    var app = angular.module('categoryManagerApp', []);
+
+    app.controller('categoryController', function($scope) {
+        $scope.models = {
+            helloAngular: 'I work!'
+        };
+    
+        categoryController.$inject = ['$scope'];
+    });
+
+
+
+})();
